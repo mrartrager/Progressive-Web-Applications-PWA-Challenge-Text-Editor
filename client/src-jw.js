4b3b73a48ef82048823ff0f0522d2b76e61948a4
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-01-21 12:19:17 -0600

    srcJW completed

--- client/src-jw.js
+++ client/src-jw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate} = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+ // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+ ({ request }) => ['style', 'script', 'worker'].includes(request.destination), // pulled from class mini project
+new StaleWhileRevalidate({
+  cacheName: 'assets-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+})
+);
